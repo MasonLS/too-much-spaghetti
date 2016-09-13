59dfe53a9ebb098ca6b2e9e24662c1cb6515c546
Aditya K <adityavkk@gmail.com>
2016-09-13 17:40:30 -0400

    fix leftover model afterCreate hook for setting isSeller true

--- server/db/models/leftover.js
+++ server/db/models/leftover.js
@@ -4,70 +4,73 @@ var _ = require('lodash');
 var Sequelize = require('sequelize');
 
 var db = require('../_db');
-var User =require('./user');
+var User = require('./user');
 var Cuisine = require('./cuisine');
 
 module.exports = db.define('leftover', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: false,
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
 
-    },
-    picture: {
-        type: Sequelize.STRING,
-        validate: {
-            isUrl: true
-        }
-    },
-    quantity: {
-        type: Sequelize.INTEGER
-    },
-    rating: {
-        type: Sequelize.INTEGER,
-    },
-    reviews: {
-        type: Sequelize.ARRAY(Sequelize.TEXT)
-    },
-    expiration_time: {
-        type: Sequelize.DATE,
-        validate: {
-            isDate: true
-        }
+  },
+  picture: {
+    type: Sequelize.STRING,
+    validate: {
+      isUrl: true
     }
+  },
+  quantity: {
+    type: Sequelize.INTEGER
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+  },
+  reviews: {
+    type: Sequelize.ARRAY(Sequelize.TEXT)
+  },
+  expiration_time: {
+    type: Sequelize.DATE,
+    validate: {
+      isDate: true
+    }
+  }
 }, {
-    classMethods: {
-      createWithCuisines: function(leftoverObj, cuisineNames){
-        let cuisinesArr;
-        const self = this;
-        return Promise.all(cuisineNames.map(function(name){
+  classMethods: {
+    createWithCuisines: function(leftoverObj, cuisineNames) {
+      let cuisinesArr;
+      const self = this;
+      return Promise.all(cuisineNames.map(function(name) {
           return Cuisine.findOrCreate({
             where: {
               cuisine: name
             }
           })
         }))
-        .then(function(cuisines){
+        .then(function(cuisines) {
           //cuisines is an array of arrays (Promise.all)!
-          cuisinesArr = cuisines.map(cuisine=>cuisine[0]);
-          console.log(cuisinesArr);
+          cuisinesArr = cuisines.map(cuisine => cuisine[0]);
           return self.create(leftoverObj);
         })
-        .then(function(leftover){
+        .then(function(leftover) {
           return leftover.addCuisines(cuisinesArr);
         })
         .catch(console.error);
-      }
-    },
-    hooks: {
-        afterCreate: function(createdLeftover){
-            User.findById(createdLeftover.chefId)
-            .then(function(user){
-                user.isSeller = true;
-            });
-        }
     }
+  },
+  hooks: {
+    afterCreate: function(createdLeftover) {
+      console.log(createdLeftover);
+      return User.findById(createdLeftover.chefId)
+        .then(function(user) {
+          return user.update({
+            isSeller: true
+          })
+        });
+    }
+  }
 });
+
