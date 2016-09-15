3eebdc6dd61dc460299d7e708da2063aee886b18
Rosana Liang <rosanajliang@gmail.com>
2016-09-15 12:36:51 -0400

    leftover detail state started

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -5,20 +5,14 @@ app.controller('HomeCtrl', function($scope, $log, CuisineFactory, LeftoverFactor
     CuisineFactory.getAll()
     .then(function(cuisines) {
         $scope.cuisines = cuisines;
-        console.log($scope.cuisines);
     })
     .catch($log.error);
 
     LeftoverFactory.getAll()
     .then(function(leftovers) {
         $scope.leftovers = leftovers;
+        $scope.featuredLeftovers = $scope.leftovers.slice(0, 3);
     })
     .catch($log.error);
-
-    $scope.shuffledLeftovers = $scope.leftovers.sort(function() {
-        return 0.5 - Math.random();
-    });
-
-    $scope.featuredLeftovers = $scope.shuffledLeftovers.slice(0, 3);
 
 });
---
+++ browser/js/leftover-detail/leftover-detail-pics.factory.js
@@ -0,0 +1,6 @@
+app.factory('LeftoverDetailPicsFactory', function () {
+    return [
+        'https://s14.postimg.io/3nsy5rfq9/9e9ce12776a29032194b2bebe4314c05.jpg',
+        'http://i2.cdn.turner.com/cnnnext/dam/assets/130123171533-gluten-pasta-plate-story-top.jpg'
+    ];
+});
