dd729feb69bc659dacadf0342dcccc5d76e6b7fb
Aditya K <adityavkk@gmail.com>
2016-09-16 16:14:44 -0400

    fix unauthorized userId bug

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -6,6 +6,13 @@ const Review = require('../../../db/models/review');
 const Leftover = require('../../../db/models/leftover');
 const Err = require('../utils/err').gen;
 
+let unauthorizedError = new Err(401, 'Unauthorized to review this product')
+
+router.use(function(req, res, next) {
+  if (!req.user) next(unauthorizedError)
+  else next();
+})
+
 router.post('/', function(req, res, next) {
   let {
     leftoverId,
@@ -22,17 +29,17 @@ router.post('/', function(req, res, next) {
       let leftovers = orders.map(or => or.leftover);
       if (leftovers.some(leftover => leftover.id === leftoverId) || req.user.isAdmin) {
         return Review.create({
-          leftoverId: leftoverId,
-          body: body,
-          stars: stars,
-          userId: userId
-        })
+            leftoverId: leftoverId,
+            body: body,
+            stars: stars,
+            userId: userId
+          })
+          .then(function(_) {
+            res.status(200).json('Review posted');
+          })
       } else {
-        next(new Err(401, 'Unauthorized to review this product'));
+        next(unauthorizedError);
       }
-    })
-    .then(function(_){
-      res.status(200).json('Review posted');
     })
 })
 
